cf5f1b8f05f3e8ca7d83741c3ef359f9632e0619
liyongqiang <liyongqiang@192.168.1.41>
2018-09-12 20:07:48 +0800

    集成log4j日志

--- springboot-backstage-rest/src/main/java/com/backend/trics/platform/rest/controller/TestDubboController.java
+++ springboot-backstage-rest/src/main/java/com/backend/trics/platform/rest/controller/TestDubboController.java
@@ -1,5 +1,6 @@
 package com.backend.trics.platform.rest.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,12 +9,15 @@ import com.backend.trics.platform.api.service.TestDubbo;
 
 @Controller
 public class TestDubboController {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestDubboController.class);
+
 	@Reference
 	private TestDubbo duboTest;
 
 	@RequestMapping("/")
 	String index() {
 		System.out.println("输出信息!输出信息!......." + duboTest.getDubboString("213123"));
+		logger.info("===================");
 		return "index";
 	}
 
